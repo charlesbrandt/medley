5b861f4a911e2f3c7fae08354bbf9486393651c4
Charles Brandt <code@charlesbrandt.com>
2014-04-19 07:29:00 -0400

    updates to launch script notes

--- launch.sh
+++ launch.sh
@@ -18,5 +18,17 @@ cd /c/medley/medley
 python application.py /c/podcasts
 
 http://localhost:8080/
+
+if added to an init script, could use:
+sudo /etc/init.d/medley start
+cat /var/log/medley.log
+cat /var/log/medley.err
+
+have also been using customized watchmedo scripts
+cd /c/medley/medley
+watchmedo tricks tricks.yaml 
+
+touch /c/medley/medley/application.py
+
 "
 
